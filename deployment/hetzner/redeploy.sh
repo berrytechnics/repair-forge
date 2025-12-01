f4ba66a70f9359ded802c949f90c9d9011d7ffd1
Kyle Berry <kyle.berry@smartwayslfl.com>
2025-12-01 09:01:23 -0600

    Update redeploy.sh

--- deployment/hetzner/redeploy.sh
+++ deployment/hetzner/redeploy.sh
@@ -4,8 +4,12 @@ cd ~/circuit-sage
 
 git pull
 
+echo "\n Pulled latest code \n"
+
 cd /opt/circuit-sage
 
 cp -r ~/circuit-sage/ ./
+
+echo "\n Copied code to /opt/circuit-sage \n"
 
 ./deployment/hetzner/deploy.sh
